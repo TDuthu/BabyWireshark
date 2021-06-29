41579362567ab25aceae7ca971e18217ddcc471b
lguillotte <76229629+lguillotte@users.noreply.github.com>
2021-06-28 20:33:24 -0500

    Update baby_wireshark.py

--- baby_wireshark.py
+++ baby_wireshark.py
@@ -1,38 +1,28 @@
 #! /usr/bin/python3
-import sys
 import re
+import sys
 from scapy.all import *
 
-#pcap_ip_grabber takes in a pcap file, grabs all IP addresses, and outputs a list of them
-def pcap_ip_grabber(input_file):
-  loop = True
-  while loop:
-    pcap_in = input('What are you looking for?\n[1] Sources\n[2] Destinations\n[3] Protocols\n[4] Keywords\n')
-    if pcap_in == '1':
-      ip_list = []
-      packets = rdpcap(input_file)
-      for packet in packets:
-        if IP in packet:
-          ip_list.append(packet[IP].src)
-      return auth_parser(ip_list)
-    elif pcap_in == '2':
-      ip_list = []
-      packets = rdpcap(input_file)
-      for packet in packets:
-        if IP in packet:
-          ip_list.append(packet[IP].dst)
-      return auth_parser(ip_list)
-    elif pcap_in == '3':
-      loop = False
-      print('<List of pcap by Protocols here>')
-    elif pcap_in == '4':
-      loop = False
-      print('<Forward over to keyword search function here>')
-    else:
-      print('Please select a menu option.')
+#main takes in a file and forwards it to the correct parser
+def main():
+  if len(sys.argv) != 2:
+    sys.exit('Please execute with one file next time.')
+  else:
+    input_file = sys.argv[1]
+    loop = True
+    while loop:
+      main_menu = input('What type of file is this?\n[1] txt\n[2] pcap\n')
+      if main_menu == '1':
+        return(txt_parser(input_file))
+      elif main_menu == '2':
+        global packets
+        packets = rdpcap(input_file)
+        return(pcap_parser(packets))
+      else:
+        print('Please select [1] or [2].')
 
-#txt_ip_grabber takes in a text file, grabs all IP addresses, and outputs a list of them
-def txt_ip_grabber(input_file):
+#txt_parser takes in a text file, grabs all IP addresses, and forwards to format_func
+def txt_parser(input_file):
   re_ip = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
   raw_list = []
   ip_list = []
@@ -45,10 +35,40 @@ def txt_ip_grabber(input_file):
   raw_list = list(filter(None, raw_list))
   #format raw_list in to a list of strings and output
   ip_list = [item for sublist in raw_list for item in sublist]
-  auth_parser(ip_list)
+  format_func(ip_list)
 
-#auth_parser will take in the IP list and count the number of times each one appears
-def auth_parser(ip_list):
+#pcap_parser takes in packets and forwards them to the correct function
+def pcap_parser(packets):
+  loop = True
+  while loop:
+    pcap_menu = input('What are you looking for?\n[1] Sources\n[2] Destinations\n[3] Protocols\n[4] IP Scanner\n')
+    if pcap_menu == '1':
+      ip_list = []
+      for packet in packets:
+        if IP in packet:
+          ip_list.append(packet[IP].src)
+      return format_func(ip_list)
+    elif pcap_menu == '2':
+      ip_list = []
+      for packet in packets:
+        if IP in packet:
+          ip_list.append(packet[IP].dst)
+      return format_func(ip_list)
+    elif pcap_menu == '3':
+      loop = False
+      print('<List of pcap by Protocols here>')
+    elif pcap_menu == '4':
+      return(ip_scanner(packets)
+    else:
+      print('Please select a valid menu option.')
+
+#def ip_search (packets):
+#  search_ip = input('Enter an IP address: ')
+#  for packet in packets:
+#    if IP in packet:
+
+#format_func will take in the IP list and count the number of times each one appears
+def format_func(ip_list):
   total = len(ip_list)
   ip_dict = {}
   sorted_dict = {}
@@ -68,18 +88,5 @@ def auth_parser(ip_list):
   print('-------------------------------------')
   print('| Total {0: >27} |'.format(str(total)))
   print('-------------------------------------')
-
-#main takes in a file, passes it through ip_grabber, then through auth_parser
-def main():
-  input_file = sys.argv[1]
-  loop = True
-  while loop:
-    input0 = input('What type of file is this?\n[1] txt\n[2] pcap\n')
-    if input0 == '1':
-      return(txt_ip_grabber(input_file))
-    elif input0 == '2':
-      return(pcap_ip_grabber(input_file))
-    else:
-      print('Please select [1] or [2].')
 
 main()
